637c9600498347c78b29c7501668a452bf87f75f
theodoreln <143392859+theodoreln@users.noreply.github.com>
2024-02-07 13:59:49 +0100

    Add files via upload

--- Assignement_A/Step_1.py
+++ Assignement_A/Step_1.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import gurobipy as gp
 GRB = gp.GRB
@@ -27,16 +28,21 @@ Generators = pd.DataFrame([
     ['Biomass',20,40],['Nuclear',80,20],
     ['Wind 1',20,0],['Wind 2',5,0]],
     columns=['Name','Capacity','Bid price'])
+# Order in ascending orders of bid price for easier treatment
+Generators = Generators.sort_values(by='Bid price').reset_index(drop=True)
 
 Demands = pd.DataFrame([
-    ['Houses',120,120],['Industry 1',60,100],
+    ['Houses',120,120],['Industry 1',50,100],
     ['Industry 2',20,80]],
     columns=['Name','Load','Offer price'])
+# Order in descending orders of offer price for easier treatment
+Demands = Demands.sort_values(by='Offer price', ascending=False).reset_index(drop=True)
 
 
-""" Optimization problem """
+""" Function useful for clearing the market """
         
-def Coplate_single_hour(Generators, Demands) :
+# Taking the hour supply and load information and optimizing (see lecture 2)
+def Single_hour_optimization(Generators, Demands) :
     # Numbers of generators and demanding units
     n_gen = len(Generators)
     n_dem = len(Demands)
@@ -66,17 +72,149 @@ def Coplate_single_hour(Generators, Demands) :
         
         # Print
         print("\n")
+        print("Power generation :")
         for i, value in enumerate(optimal_gen):
-            print(f"Generator {i}: {value}")
+            print(Generators["Name"][i] + f" : {value}")
+        print("\n")
+        print("Demand provided :")
         for i, value in enumerate(optimal_dem):
-            print(f"Demand {i}: {value}")
+            print(Demands["Name"][i] + f" : {value}")
     else:
         print("Optimization did not converge to an optimal solution.")
         
     # Return the cost and the optimal values
     return(optimal_obj, optimal_gen, optimal_dem)
     
-optimal_obj, optimal_gen, optimal_dem = Coplate_single_hour(Generators, Demands)        
+
+# Taking the optimization and giving the clearing price
+def Single_hour_price(Generators, Demands, optimal_gen, optimal_dem) :
+    # Go through the different suppliers to find the clearing price
+    clearing = False
+    clearing_price = 0
+    i = 0
+    while (clearing == False) and (i <= len(Generators)) :
+        max_cap = Generators['Capacity'][i]
+        eff_cap = optimal_gen[i]
+        if eff_cap > 0 and eff_cap < max_cap :
+            clearing_price = Generators['Bid price'][i]
+            clearing = True
+        elif eff_cap == max_cap and i!= len(Generators)-1 :
+            next_eff_cap = optimal_gen[i+1]
+            if next_eff_cap != 0 :
+                clearing_price = Generators['Bid price'][i]
+                i += 1
+            else :
+                next_bid_price = Generators['Bid price'][i+1]
+                clearing_price = [clearing_price, next_bid_price]
+                clearing = True
+        else :
+            clearing_price = Generators['Bid price'][i]
+            clearing = True
+            
+    print("\n")
+    print(f"Clearing price : {clearing_price} $/MWh")
+    print("Quantity provided : " + str(sum(optimal_dem)) + " MW")
+    return(clearing_price)
+
+
+# Plotting the solution of the clearing for an hour and demands and generators entries
+def Single_hour_plot(Generators, Demands, clearing_price, optimal_gen, optimal_dem) :
+    # Size of the figure
+    plt.figure(figsize = (20, 12))
+    plt.rcParams["font.size"] = 16
+    
+    # Different colors for each suppliers
+    colors = sns.color_palette('flare', len(Generators))
+    # Positions of the suppliers bars
+    xpos = [0]
+    for i in range(1,len(Generators)) :
+        xpos.append(Generators["Capacity"][i-1] + xpos[i-1])
+    y = Generators["Bid price"].values.tolist()
+    # Width of each suppliers bars
+    w = Generators["Capacity"].values.tolist()
+
+    fig = plt.bar(xpos, 
+            height = y,
+            width = w,
+            fill = True,
+            color = colors,
+            align = 'edge')
+
+    plt.xlim(0, max(Generators["Capacity"].sum(),Demands["Load"].sum()+5))
+    plt.ylim(0, max(Generators["Bid price"].max(),Demands["Offer price"].max()) + 15)
+    
+    plt.legend(fig.patches, Generators["Name"].values.tolist(),
+              loc = "best",
+              ncol = 3)
+    
+    
+    # Demands
+    max_demand = sum(Demands["Load"].values.tolist())
+    xpos = 0
+    for i in range(len(Demands)) :
+        plt.hlines(y = Demands["Offer price"][i],
+                  xmin = xpos,
+                  xmax = Demands["Load"][i] + xpos,
+                  color = "red",
+                  linestyle = "dashed")
+        xpos = Demands["Load"][i] + xpos
+        if i != len(Demands)-1:
+            plt.vlines(x = xpos,
+                        ymin = Demands["Offer price"].values.tolist()[i+1],
+                        ymax = Demands["Offer price"].values.tolist()[i],
+                        color = "red",
+                        linestyle = "dashed",
+                        label = "Demand")
+    plt.vlines(x = max_demand,
+                ymin = 0,
+                ymax = Demands["Offer price"].values.tolist()[-1],
+                color = "red",
+                linestyle = "dashed",
+                label = "Demand")
+    
+    if type(clearing_price) == list :
+        plt.text(x = sum(optimal_gen) - 10,
+                y = clearing_price[-1] + 10,
+                s = f"Electricity price: \n    {clearing_price} $/MWh \n Quantity : " + str(sum(optimal_dem)) + " MW")
+    else :
+        plt.text(x = sum(optimal_gen) - 10,
+                y = clearing_price + 10,
+                s = f"Electricity price: \n    {clearing_price} $/MWh \n Quantity : " + str(sum(optimal_dem)) + "MW")
+
+    plt.xlabel("Power plant capacity (MW)")
+    plt.ylabel("Bid price ($/MWh)")
+    plt.title("Market clearing for the copper plate single hour")
+    plt.show()
+    
+
+
+""" Global function """
+
+def Copper_plate_single_hour(Generators, Demands) :
+    # Solving the optimization problem
+    optimal_obj, optimal_gen, optimal_dem = Single_hour_optimization(Generators, Demands)
+    # Clearing the price
+    clearing_price = Single_hour_price(Generators, Demands, optimal_gen, optimal_dem)
+    # Plotting the results
+    Single_hour_plot(Generators, Demands, clearing_price, optimal_gen, optimal_dem)
+    
+Copper_plate_single_hour(Generators, Demands)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         
 

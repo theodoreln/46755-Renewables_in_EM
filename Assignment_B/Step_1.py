07cd0c2055d9e6d459bad6c620ddc5fca2a297b4
theodoreln <143392859+theodoreln@users.noreply.github.com>
2024-04-22 11:10:25 +0200

    Add files via upload

--- Assignment_B/Step_1.py
+++ Assignment_B/Step_1.py
@@ -35,7 +35,7 @@ def Offering_one_price(in_sample) :
     
     # Add the objective function to the model, sum on every t, separation of the conventional generation units and the wind farms
     model.setObjective(gp.quicksum(gp.quicksum((1/n_scen)*(in_sample['DA_price'][w][t]*var_qu_off[t] + in_sample['Binary_var'][w][t]*0.9*in_sample['DA_price'][w][t]*var_qu_diff[t,w]
-                                                           + (1-in_sample['Binary_var'][w][t])*0.9*in_sample['DA_price'][w][t]*var_qu_diff[t,w]) for w in range(n_scen)) for t in range(n_hour)), GRB.MAXIMIZE)
+                                                           + (1-in_sample['Binary_var'][w][t])*1.2*in_sample['DA_price'][w][t]*var_qu_diff[t,w]) for w in range(n_scen)) for t in range(n_hour)), GRB.MAXIMIZE)
     
     #Add constraints to the model
     for t in range(n_hour):
@@ -122,5 +122,79 @@ def Offering_two_price(in_sample) :
     
     return(optimal_qu_off, optimal_obj)
 
-
 optimal_qu_off_two, optimal_obj_two = Offering_two_price(in_sample)
+
+
+##############################################
+""" Compute the profits for each scenarios """
+##############################################
+
+def Profits_scenarios(in_sample, optimal_qu_off, price_scheme) :
+    #Number of units to take into account (based on data)
+    # Number of scenarios
+    n_scen = len(in_sample)
+    # Number of hour
+    n_hour = 24
+    
+    # Creating the list of profits
+    profit = [0]*n_scen
+    
+    # Compute the profit for each scenarios
+    for w in range(n_scen) :
+        profit_w = 0
+        for t in range(n_hour) :
+            optimal_qu_diff = in_sample['DA_forecast'][w][t] - optimal_qu_off[t]
+            # Profit depends on the price scheme
+            if price_scheme == 1 :
+                profit_w += in_sample['DA_price'][w][t]*optimal_qu_off[t] + in_sample['Binary_var'][w][t]*0.9*in_sample['DA_price'][w][t]*optimal_qu_diff + (1-in_sample['Binary_var'][w][t])*1.2*in_sample['DA_price'][w][t]*optimal_qu_diff
+            elif price_scheme == 2 :
+                profit_w +=in_sample['DA_price'][w][t]*optimal_qu_off[t] 
+                if in_sample['Binary_var'][w][t] == 1 :
+                    if optimal_qu_diff >= 0 :
+                        profit_w += 0.9*in_sample['DA_price'][w][t]*optimal_qu_diff
+                    else :
+                        profit_w += in_sample['DA_price'][w][t]*optimal_qu_diff
+                else :
+                    if optimal_qu_diff >= 0 :
+                        profit_w += in_sample['DA_price'][w][t]*optimal_qu_diff
+                    else :
+                        profit_w += 1.2*in_sample['DA_price'][w][t]*optimal_qu_diff
+                    
+        # Save the profit for scenario w
+        profit[w] = profit_w
+    
+    return(profit)
+    
+profit_one = Profits_scenarios(in_sample, optimal_qu_off_one, 1)
+profit_two = Profits_scenarios(in_sample, optimal_qu_off_two, 2)
+
+def Show_distribution(profit, nb_bins) :
+    
+    # Create histogram
+    plt.hist(profit, bins=nb_bins, color='blue', edgecolor='black')
+    
+    # Add labels and title
+    plt.xlabel('Profit')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of profits')
+    
+    # Display the plot
+    plt.show()
+    
+Show_distribution(profit_one, 80)
+Show_distribution(profit_two, 80)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
